91ea66d0d68d7b0368a6f366d50d24ba5604fa81
Vinicius <viniciusoliveira1001@gmail.com>
2019-10-10 11:43:57 -0300

    Update Arm.py

--- pythonClass/Arm.py
+++ pythonClass/Arm.py
@@ -1,4 +1,6 @@
 import serial
+import numpy as np
+
 
 
 class Arm:
@@ -25,6 +27,39 @@ class Arm:
     def junta3(self, angulo3):
         self.conexao.write(b'G1 C', self.angulo3)
 
+
+    def read(self, size):
+        self.conexao.read(size)
+
+
+    def sendMsgNoWait(self, _msg)
+        read(4)
+
+        _msg += ";"
+        print(_msg)
+        send(_msg)
+
+
+    def sendMsg(self, _msg)
+        read(4)
+
+        _msg += ";"
+        print(_msg)
+        send(_msg)
+
+        read(1)
+
+
+    def sendMoveNoWait(self, _value,  _joint):
+        msg = "G1 "
+        joint = chr( 65 + _joint)
+        msg += joint
+        msg += str(_value)
+        msg += ';'
+        print(msg)
+        send(msg)
+
+
     def sendMove(self, _value,  _joint):
         msg = "G1 "
         joint = chr( 65 + _joint)
@@ -33,6 +68,44 @@ class Arm:
         msg += ';'
         print(msg)
         send(msg)
-        # TODO: Adicionar while travante
 
+        read(2)
+
+
+    def sendMoveMultiNoWait(value, joint,size):
+        read(4)
+
+        msg = "G1 "
+        for i in range(0,size):
+            joint = chr( 65 + joint[i])
+            msg += joint
+            msg += str(value[i])
+            msg += ' '
+
+        msg+=";"
+        print(msg)
+        send(msg)
+
+
+    def sendMoveMulti(value, joint,size):
+        read(4)
+
+        msg = "G1 "
+        for i in range(0,size):
+            joint = chr( 65 + joint[i])
+            msg += joint
+            msg += str(value[i])
+            msg += ' '
+
+        msg+=";"
+        print(msg)
+        send(msg)
+
+        read(2)
+
+
+    def multiMatrix(self, A, B, R)
+        R = np.zeros((4,4), dtype=np.double)
+        #por padrão o tipo é float64
+        R = np.dot(A,B)
 
